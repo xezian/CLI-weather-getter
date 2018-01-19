83ec5524d05ee6dee4aa7e4443b7bad5e4c48807
Brandon Haines <haines.brandon.91@gmail.com>
2018-01-18 20:44:37 -0700

    updated format

--- user.js
+++ user.js
@@ -7,9 +7,11 @@ var UserSearch = function(name, location) {
   this.getWeather = function() {
     weather.find({search: this.location, degreeType: 'F'}, function(err, result) {
       if(err) console.log(err);
+      console.log("==========================================");
       console.log(`Where: ${JSON.stringify(result[0].location.name)}`);
       console.log(`Temp: ${JSON.stringify(result[0].current.temperature)}`);
       console.log(`Conditions: ${JSON.stringify(result[0].current.skytext)}`);
+      console.log("==========================================");
       });
   };
 };
